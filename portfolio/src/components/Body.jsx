8099e1c3a45873a17f1b7f9cf6446796198f4d22
ravi <ravibadami191@gmail.com>
2024-01-13 23:01:12 +0530

    Set up complete

---
+++ portfolio/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import Header from './Header/Index';
+
+const Body = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Body;
---
+++ portfolio/src/components/Header/Index.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className='text-red-500'>Index</div>;
+};
+
+export default Header;
