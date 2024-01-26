97f4ca5c7cc38a900134665c32fb689bf5280cf8
Ravi-Badami <ravibadami191@gmail.com>
2024-01-26 19:35:34 +0530

    added social media links

--- portfolio/src/components/Contact/Index.jsx
+++ portfolio/src/components/Contact/Index.jsx
@@ -1,8 +1,11 @@
+import SocialMedia from './SocialMedia';
+
 const Contact = () => {
   return (
     <div id='messages'>
-      <section className='text-gray-600 body-font relative bg-blue-50 rounded-xl '>
-        <div className=' px-5 py-10 md:py-24 mx-auto'>
+      <section className='text-gray-600 body-font relative bg-blue-100 rounded-xl '>
+        <SocialMedia />
+        <div className=' px-5 py-10 md:py-10 mx-auto'>
           <div className='flex flex-col text-center w-full mb-6 md:mb-12'>
             <h1 className='text-3xl md:text-6xl  title-font font-extrabold mb-4 text-gray-900'>
               Lets connect
@@ -36,11 +39,11 @@ const Contact = () => {
                   <textarea
                     id='message'
                     name='message'
-                    className='w-full min-h-44 bg-gray-100 bg-opacity-50 rounded border border-black focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-auto text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out resize-both'></textarea>
+                    className='w-full min-h-44 bg-gray-100 bg-opacity-50 rounded border border-black focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-auto text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out resize-both'></textarea>
                 </div>
               </div>
               <div className='p-2 w-full'>
-                <button className='flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
+                <button className='flex mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg'>
                   Button
                 </button>
               </div>
