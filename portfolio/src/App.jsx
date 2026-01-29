4dbc002f88fe526d436a3346ec82df854ed86d1a
Ravikumar Badami <ravibadami191@gmail.com>
2026-01-30 00:04:05 +0530

    added web analytics

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,12 @@
 import Body from './components/Body';
 
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <>
       <Body />
+      <Analytics />
     </>
   );
 }
